4268d3bc3bf0cb50700cf9949b758a304db7cc75
Senni K Kaipio <skaipio@svm-19.cs.helsinki.fi>
2014-07-14 15:33:21 +0300

    Project frame

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ohtuProjektiAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ohtuProjektiAppApp
+ */
+angular.module('ohtuProjektiAppApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
