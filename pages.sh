4b78f54d4d461e1bc1f2e23f58c2238f81f316a9
Yessergire Mohamed <yessergire.mohamed@helsinki.fi>
2014-07-15 14:45:37 +0300

    github pages

---
+++ pages.sh
@@ -0,0 +1,17 @@
+# Siirry gh-pages branchiin
+git checkout gh-pages
+
+echo tyhjennä kansio
+git rm -r .
+
+echo kopio .gitignore
+git checkout master .gitignore
+
+echo kopio kaikki tiedostot dist-kansiosta tänne
+cp -R dist/* .
+
+echo lisää, ja committaa
+git add .
+git commit -m "pages"
+git push origin gh-pages
+git checkout master
