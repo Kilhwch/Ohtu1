a66d737606ab1202690924e65266daacecce2595
pauge <arto.kiviluoto@gmail.com>
2014-07-16 09:23:35 +0300

    Data can be retrieved from GitHub

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,30 @@
  * # MainCtrl
  * Controller of the ohtuProjektiAppApp
  */
-angular.module('ohtuProjektiAppApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
+var myApp = angular.module('ohtuProjektiAppApp');
+
+ myApp.config(function($httpProvider){
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+});
+
+myApp.controller('MainCtrl', function ($scope,$http) {
+    $http.get('https://api.github.com/repos/pauge/TiraLabra/issues/2').success(function(issue)
+    {
+      
+      $scope.task = issue;
+    }).error(function() {
+        $scope.task = {id: "Virhe"}
+    })
+    
+    //var xhr = new XMLHttpRequest();
+    //xhr.open('GET', 'https://github.com/pauge/TiraLabra/issues/2?');
+    //xhr.onreadystatechange = function () {
+      //if (this.status == 200 && this.readyState == 4) {
+        //console.log('response: ' + this.responseText);
+      //}
+    //};
+    //xhr.send();
   });
+  
