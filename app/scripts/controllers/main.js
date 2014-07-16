d63d51cf038e25c814e216bb968c29359473f29f
acidi <acidi@do-not-reply.com>
2014-07-16 14:49:56 +0300

    github authentication works

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,6 +16,20 @@ myApp.controller('MainCtrl', function ($scope,$http) {
       $scope.tasks = issue;
     }).error(function() {
         $scope.task = {id: "Virhe"}
-    })
+    });
+   OAuth.initialize('CHkmXQc9pfI3vqPZectNDagrwSc');
+   OAuth.popup('github')
+      .done(function(results) {
+        result.get('https://api.github.com/repos/Kilhwch/Ohtu1/issues/1')
+        .done(function (response) {
+          console.log(response);
+        })
+        .fail(function (err) {
+          console.log(err);
+        });
+      })
+      .fail(function (err) {
+        console.log(err);
+      });
   });
   
