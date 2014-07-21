47a6135be1bd1cb31d8b5ed449562f443c470e68
Yessergire Mohamed <yessergire.mohamed@helsinki.fi>
2014-07-21 14:33:00 +0300

    updated header

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -9,11 +9,18 @@
  */
 
 angular.module('ohtuProjektiAppApp')
-  .controller('HeaderCtrl', function($scope, github) {
+  .controller('HeaderCtrl', function($scope, $location, github) {
+    $scope.url = $location.url();
+    $scope.list = [{name: 'Home', url : '/main'}, {name: 'List', url: '/list'}];
     if (github.isAuthenticated()) {
-      $scope.link = "logout";
+      $scope.list.push({name: 'Logout', url: '/logout'});
     } else {
-      $scope.link = "login";
+      $scope.list.push({name: 'Login', url: '/login'});
+    }
+    for (var i = 0; i < $scope.list.length; i++) {
+      if ($scope.list[i].url === $scope.url) {
+         $scope.list[i].class = "active";
+      }
     }
   });
 
