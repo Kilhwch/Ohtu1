af5706f1413d4468c2901cad1376f9ff8c710ee3
Kasper A Kivikataja <kkivikat@dat-7.cs.helsinki.fi>
2014-07-22 12:18:55 +0300

    CreateIssueTest

--- app/scripts/controllers/issues.js
+++ app/scripts/controllers/issues.js
@@ -4,15 +4,26 @@ var myApp = angular.module('ohtuProjektiAppApp');
      
 myApp.controller('IssuesCtrl', function ($scope, $stateParams, github) {
         $scope.issues = new github.Issue($stateParams.owner, $stateParams.repoName);
+        
+        // list issues
         $scope.issues.list({}, function(data) {
         $scope.issues = data;
-});
-        $scope.issues.list({}, function(error, data) {
-        console.log('createIssue');
-        if (error)
-            console.error(error);
-            console.dir(data);
-	});
+    });
+        // get a specific issue
+        
+        // koodia
+	
+	    // create a new issue
+	    $scope.addIssue = function() {
+	       alert("haloo");
+	       var d = {title: $scope.issueTitle, body: "hello world heheh"}; 
+	       var options = { method: "POST", url: "https://api.github.com/repos/Kilhwch/Ohtu1/issues", data: d };
+	        $scope.issues.createIssue(options, function(data) {
+                console.log(data);
+            });
+	    };
+	
+
 });
 
 
