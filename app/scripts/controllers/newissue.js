11a0fb7c8550eb2eb8bfdf4ca6a92b6d720c9e4d
acidi <acidi@do-not-reply.com>
2014-07-23 09:29:47 +0300

    newissue fields clear after post

--- app/scripts/controllers/newissue.js
+++ app/scripts/controllers/newissue.js
@@ -11,12 +11,14 @@ angular.module('ohtuProjektiAppApp')
   .controller('NewissueCtrl', function ($scope,github,$stateParams) {
           $scope.issues = new github.Issue($stateParams.owner, $stateParams.repoName);
           $scope.addIssue = function() {
-          console.log("addIssue kutsuttu");
-          var d = {title: $scope.issueTitle, body: $scope.issueBody};
-          $scope.issues.createIssue(d, function(err, data) {
+            console.log("addIssue kutsuttu");
+            var d = {title: $scope.issueTitle, body: $scope.issueBody};
+            $scope.issues.createIssue(d, function(err, data) {
                 console.log("createIssue kutsuttu");
                 console.log(err);
                 console.log(data);
             });
+            $scope.issueTitle = "";
+            $scope.issueBody = "";
           };
       });
