246134a6cebebfccd814a25446e2cfa535f176c2
acidi <acidi@do-not-reply.com>
2014-07-23 10:13:48 +0300

    issues.js moved to issueboard.js

--- app/scripts/controllers/issueboard.js
+++ app/scripts/controllers/issueboard.js
@@ -8,8 +8,28 @@
  * Controller of the ohtuProjektiAppApp
  */
 angular.module('ohtuProjektiAppApp')
-  .controller('IssueboardCtrl', function ($scope, $stateParams) {
-    $scope.repoName = $stateParams.repoName;
-    $scope.owner = $stateParams.owner;
+  .controller('IssueboardCtrl', function ($scope, $stateParams,github) {
+    $scope.milestones = new github.Milestone($stateParams.owner, $stateParams.repoName);
+    $scope.milestones.list({}, function(data) {
+        $scope.milestones = data;
+        console.log(data);
+    });
+    $scope.milestones.list({}, function(error, data) {
+        console.log('createMilestone');
+        if (error)
+            console.error(error);
+        console.dir(data);
+    });
+
+    $scope.issues = new github.Issue($stateParams.owner, $stateParams.repoName);
+    $scope.issues.list({}, function(data) {
+        $scope.issues = data;
+    });
+    $scope.issues.list({}, function(error, data) {
+        console.log('createIssue');
+        if (error)
+            console.error(error);
+        console.dir(data);
+    });
 
   });
