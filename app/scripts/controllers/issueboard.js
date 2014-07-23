c900e2ca078cfede26b3237e8478d3758c590f82
acidi <acidi@do-not-reply.com>
2014-07-23 10:26:19 +0300

    removed examples from issueboard.js

--- app/scripts/controllers/issueboard.js
+++ app/scripts/controllers/issueboard.js
@@ -14,22 +14,9 @@ angular.module('ohtuProjektiAppApp')
         $scope.milestones = data;
         console.log(data);
     });
-    $scope.milestones.list({}, function(error, data) {
-        console.log('createMilestone');
-        if (error)
-            console.error(error);
-        console.dir(data);
-    });
-
     $scope.issues = new github.Issue($stateParams.owner, $stateParams.repoName);
     $scope.issues.list({}, function(data) {
         $scope.issues = data;
-    });
-    $scope.issues.list({}, function(error, data) {
-        console.log('createIssue');
-        if (error)
-            console.error(error);
-        console.dir(data);
     });
 
   });
