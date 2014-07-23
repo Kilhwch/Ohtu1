c483af259f0697e72b03cdea132aa9eec3612134
Joonas K Longi <jlongi@dat-5.cs.helsinki.fi>
2014-07-23 12:41:07 +0300

    protractor

--- test/spec/views/issueboard.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-describe('TODO list', function() {
-    it('should have all tasts', function() {
-      browser.get('http://localhost:38765/#/repos/Kilhwch/Ohtu1');
-      expect(element.all(by.repeater('issue in issues')).count()).toEqual(10);
-    });
-});
-
