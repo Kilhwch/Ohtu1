006d8c6ec495abffe78a3918133e4c953a97179d
Senni Kaipio <senni.kaipio@helsinki.fi>
2014-07-24 13:18:10 +0300

    Add a few Issuebox e2e edit tests.

--- test/mocked-backend.js
+++ test/mocked-backend.js
@@ -3,13 +3,13 @@ exports.httpBackendMock = function() {
     .run(function($httpBackend) {
         var apiUrl = 'https://api.github.com';
         var list = apiUrl + '/user/repos';
-	var issueboard = apiUrl + '/repos/user/repo/issues';
+	    var issueboard = apiUrl + '/repos/user/repo/issues';
         var repo = {name: 'Test Repo', description: 'Test description'};
-	var issue = {title: 'Test tickle'};
+	    var issue = {title: 'Test tickle'};
 
         $httpBackend.whenGET(/https:\/\/oauth.io.*/).respond('token');        
         $httpBackend.whenGET(list).respond([repo]);
-	$httpBackend.whenGET(issueboard).respond([issue]);        
+	    $httpBackend.whenGET(issueboard).respond([issue]);        
         $httpBackend.whenGET(/.*/).passThrough();
-        });
+    });
 }
