8baeb3a23be709607fde41ef5518dfca5e61f74f
Yessergire Mohamed <yessergire.mohamed@helsinki.fi>
2014-07-24 09:53:55 +0300

    failing protractor test will fail grunt test

--- Gruntfile.js
+++ Gruntfile.js
@@ -361,7 +361,7 @@ module.exports = function (grunt) {
     protractor: {
       options: {
         configFile: "protractor.conf.js", // Default config file
-        keepAlive: true, // If false, the grunt process stops when the test fails.
+        keepAlive: false, // If false, the grunt process stops when the test fails.
         noColor: false, // If true, protractor will not use colors in its output.
         args: {
           seleniumServerJar: 'node_modules/protractor/selenium/selenium-server-standalone-2.42.2.jar',
