02dbf630dd90f9e4d04f6078b10799e5a1fe907b
Yessergire Mohamed <yesser.gire@gmail.com>
2014-07-31 12:30:35 +0300

    grunt:watch configuration updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,14 +34,21 @@ module.exports = function (grunt) {
         tasks: ['wiredep']
       },
       js: {
-        files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
+        files: [
+          '<%= yeoman.app %>/scripts/**/*.js',
+          '!<%= yeoman.app %>/scripts/**/*_spec.js',
+          '!<%= yeoman.app %>/scripts/**/*_test.js'
+        ],
         tasks: ['newer:jshint:all'],
         options: {
           livereload: '<%= connect.options.livereload %>'
         }
       },
       jsTest: {
-        files: ['test/spec/{,*/}*.js'],
+        files: [
+          '<%= yeoman.app %>/scripts/**/*_spec.js',
+          '<%= yeoman.app %>/scripts/**/*_test.js'
+        ],
         tasks: ['newer:jshint:test', 'karma']
       },
       styles: {
