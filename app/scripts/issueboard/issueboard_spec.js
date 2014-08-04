5ee5b37e5e3823bd358b2ceabd7fc22b533edd56
Joonas K Longi <jlongi@dat-5.cs.helsinki.fi>
2014-08-04 13:23:53 +0300

    se on ny chrome pojat

--- app/scripts/issueboard/issueboard_spec.js
+++ app/scripts/issueboard/issueboard_spec.js
@@ -127,27 +127,21 @@ describe('Listing issues', function() {
       alertDialog.accept();
       expect(alertDialog.getText()).toContain("Created label: label name");
     });
-
-    xit('should edit label of issue', function(){
-        var backlog = element.all((by.css('.backlogbox'))).get(0);
-        expect(backlog.getText()).toContain('Test tickle')
-    
-        var issueElem = element.all(by.repeater('issue in issues')).first();
-        var notEdit = issueElem.element(by.css('.notedit'));
-        notEdit.click();
-
-        issueElem.element(by.id('labels')).click();
-        issueElem.element(by.css('#labels option[value="0"]')).click();
-
-        ptor.sleep(1000);
-
-        var ready = element.all((by.css('.readybox'))).get(1);
-        expect(ready.getText()).toContain('Test tickle')
-
-    });
   });
 
   describe('Issue box', function(){
+
+    it('should edit label of issue', function(){
+        var backlog = element.all((by.css('.backlogbox'))).get(0);
+        expect(backlog.getText()).toContain('Test tickle')
+        var issueElem = element.all(by.repeater('issue in issues')).first();
+        var notEdit = issueElem.element(by.css('.notedit'));
+        notEdit.click();
+        issueElem.element(by.id('labels')).click();
+        issueElem.element(by.css('#labels option[value="0"]')).click();
+        var done = element.all((by.css('.donebox'))).get(0);
+        expect(done.getText()).toContain('Test tickle');
+    });
 
     it('should not be in edit mode when first viewing backlog', function(){
       element.all(by.repeater('issue in issues')).each(function(elem){
