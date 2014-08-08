ef61e5e014c688913744fc77bd2b7d883f4b4e59
Yessergire Mohamed <yesser.gire@gmail.com>
2014-08-08 10:47:21 +0300

    milestone tests added

---
+++ app/scripts/issueboard/newmilestone/newmilestone_spec.js
@@ -0,0 +1,32 @@
+describe('Creating milestones', function() {
+
+  var mockModule = require('../../mocked-backend'),
+      authModule = require('../../mocked-auth'),
+      ptor;
+
+  beforeEach(function() {
+    ptor = protractor.getInstance();
+    ptor.addMockModule('httpBackendMock', mockModule.httpBackendMock);
+    ptor.addMockModule('auth', authModule.authMock);
+    browser.get('#/logout');
+    $('.login-button').click();
+    browser.get('#/repos/user/repo');
+  });
+
+  describe('when create', function() {
+
+    it('should receive correct response when creating a new milestone', function() {
+      $('#add').click();
+      element(by.id('New milestone')).click();
+      element(by.model('milestone.title')).sendKeys('milestone title');
+      element(by.model('milestone.description')).sendKeys('milestone description');
+      element(by.model('milestone.due_on')).sendKeys('milestone due on');
+      $('#newmilestone-create').click();
+      var alertDialog = ptor.switchTo().alert();
+      alertDialog.accept();
+      expect(alertDialog.getText()).toContain("Created milestone: milestone title");
+    });
+
+  });
+
+});
